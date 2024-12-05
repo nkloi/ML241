40d8cb4750ddbe19a15475bef7fca1e196eb0e26
Your Name <buikhanhhoang2003@gmail.om>
2024-12-06 03:53:47 +0700

    Add N12

---
+++ N12/flaskapp.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, Response,jsonify,request,session
+
+from flask_wtf import FlaskForm
+
+from wtforms import FileField, SubmitField,StringField,DecimalRangeField,IntegerRangeField
+from werkzeug.utils import secure_filename
+from wtforms.validators import InputRequired,NumberRange
+import os
+
+
+# Required to run the YOLOv8 model
+import cv2
+
+
+from YOLO_Video import video_detection
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'muhammadmoin'
+app.config['UPLOAD_FOLDER'] = 'static/files'
+
+
+#Use FlaskForm to get input video file  from user
+class UploadFileForm(FlaskForm):
+    file = FileField("File",validators=[InputRequired()])
+    submit = SubmitField("Run")
+
+
+def generate_frames(path_x = ''):
+    yolo_output = video_detection(path_x)
+    for detection_ in yolo_output:
+        ref,buffer=cv2.imencode('.jpg',detection_)
+
+        frame=buffer.tobytes()
+        yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame +b'\r\n')
+
+def generate_frames_web(path_x):
+    yolo_output = video_detection(path_x)
+    for detection_ in yolo_output:
+        ref,buffer=cv2.imencode('.jpg',detection_)
+
+        frame=buffer.tobytes()
+        yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame +b'\r\n')
+
+@app.route('/', methods=['GET','POST'])
+@app.route('/home', methods=['GET','POST'])
+def home():
+    session.clear()
+    return render_template('indexproject.html')
+
+@app.route("/webcam", methods=['GET','POST'])
+
+def webcam():
+    session.clear()
+    return render_template('ui.html')
+@app.route('/FrontPage', methods=['GET','POST'])
+def front():
+    # Upload File Form: Create an instance for the Upload File Form
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        
+        file = form.file.data
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
+                               secure_filename(file.filename)))  
+        
+        session['video_path'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
+                                             secure_filename(file.filename))
+    return render_template('videoprojectnew.html', form=form)
+@app.route('/video')
+def video():
+    
+    return Response(generate_frames(path_x = session.get('video_path', None)),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# To display the Output Video on Webcam page
+@app.route('/webapp')
+def webapp():
+   
+    return Response(generate_frames_web(path_x=0), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(debug=True)
